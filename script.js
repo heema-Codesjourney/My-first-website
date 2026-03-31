922f2d77671b42a61f525cbc99c3829a33ad85a4
Heema kumari <heemakumari153@gmail.com>
2026-03-31 12:03:57 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,34 +1,27 @@
-let heading = document.createElement("h1");
-heading.innerText = "My Image Gallery 🚀";
-heading.style.textAlign = "center";
-document.body.appendChild(heading);
+fetch("https://picsum.photos/200/300")
+  .then((res) => {
+    let img1 = document.createElement("img");
+    img1.src = res.url;
+    img1.style.margin = "10px";
+    document.body.appendChild(img1);
+  });
 
 
-let para = document.createElement("p");
-para.innerText = "This gallery is created using JavaScript DOM manipulation.";
-para.style.textAlign = "center";
-document.body.appendChild(para);
+// Image 2
+fetch("https://picsum.photos/200/300")
+  .then((res) => {
+    let img2 = document.createElement("img");
+    img2.src = res.url;
+    img2.style.margin = "10px";
+    document.body.appendChild(img2);
+  });
 
 
-let images = [
-  "https://global.discourse-cdn.com/sitepoint/original/3X/b/5/b59a78e2ed76c705f3c0dcb300f3f222aefdcd99.png",
-  "https://kajabi-storefronts-production.kajabi-cdn.com/kajabi-storefronts-production/blogs/13995/images/C2GPC1ZSsqJfE7mzrmAV_4-pillars-of-teamwork.jpeg",
-  "https://t3.ftcdn.net/jpg/16/93/52/74/360_F_1693527478_Li1dICP6Ovueidc11g7jTsSa1wxXBKut.jpg"
-];
-
-
-let container = document.createElement("div");
-container.style.textAlign = "center";
-
-
-images.forEach((img) => {
-  let image = document.createElement("img");
-  image.src = img;
-  image.style.width = "200px";
-  image.style.margin = "10px";
-  image.style.borderRadius = "10px";
-  container.appendChild(image);
-});
-
-
-document.body.appendChild(container);
+// Image 3
+fetch("https://picsum.photos/200/300")
+  .then((res) => {
+    let img3 = document.createElement("img");
+    img3.src = res.url;
+    img3.style.margin = "10px";
+    document.body.appendChild(img3);
+  });
