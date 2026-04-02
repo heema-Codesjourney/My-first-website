e8326aae6a29457a220c1e9760c3c9f2facb2788
Heema kumari <heemakumari153@gmail.com>
2026-04-02 22:55:44 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,8 @@
-fetch("https://picsum.photos/200/300")
-  .then((res) => {
-    let img1 = document.createElement("img");
-    img1.src = res.url;
-    img1.style.margin = "10px";
-    document.body.appendChild(img1);
-  });
-
-
-// Image 2
-fetch("https://picsum.photos/200/300")
-  .then((res) => {
-    let img2 = document.createElement("img");
-    img2.src = res.url;
-    img2.style.margin = "10px";
-    document.body.appendChild(img2);
-  });
-
-
-// Image 3
-fetch("https://picsum.photos/200/300")
-  .then((res) => {
-    let img3 = document.createElement("img");
-    img3.src = res.url;
-    img3.style.margin = "10px";
-    document.body.appendChild(img3);
-  });
+for (let i = 0; i < 100; i++) {
+  fetch("https://picsum.photos/200/300")
+    .then((res) => {
+      let img = document.createElement("img");
+      img.src = res.url;
+      document.body.appendChild(img);
+    });
+}
